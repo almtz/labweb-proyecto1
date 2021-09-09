208d8ebe788eb01846465901ebb53c024054a1ec
Alejandro López Martínez <alejandrolm14@gmail.com>
2021-09-09 10:44:43 -0500

    savepoint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../scss/main.scss";
+import type { AppProps } from "next/app";
+import { firebaseInit } from "../utils/firebase";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+firebaseInit();
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>Lab Web Proyecto 1</title>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
+};
+
+export default MyApp;
---
+++ utils/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvRorm6lzRrY6tt1HThF1IKSrDStS11z8",
+  authDomain: "labweb-proyecto1.firebaseapp.com",
+  projectId: "labweb-proyecto1",
+  storageBucket: "labweb-proyecto1.appspot.com",
+  messagingSenderId: "738209799033",
+  appId: "1:738209799033:web:24bc7358a38ae587d4ada8",
+};
+
+export const firebaseInit = () => {
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+};
