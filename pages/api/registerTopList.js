7b78c558351fcca050961e78999d025d79b3295c
Yugnar <yugnarcorp@hotmail.com>
2021-09-17 18:46:22 -0500

    Create TopList Front
    title

--- pages/api/registerTopList.js
+++ pages/api/registerTopList.js
@@ -1,4 +1,6 @@
 export default function handler(req, res) {
-    console.log("Hello");
+    console.log(req.body.name);
+    console.log(req.body.info);
+    console.log(req.body.desc);
     res.status(200).json({ user: 'Ada Lovelace' })
 }
--- pages/newTopList.jsx
+++ pages/newTopList.jsx
@@ -1,11 +1,60 @@
+import { useState } from "react";
+import {
+    Box,
+    Button,
+    Input,
+    InputLabel,
+    FormHelperText,
+    FormControl,
+    TextField,
+    Grid,
+    Paper,
+    Typography,
+    makeStyles
+  } from "@material-ui/core";
+import '@fontsource/roboto/400.css';
+
 const NewTopList = () => {
 
-    const testFunction = async event => {
+    const useStyles = makeStyles((theme) => ({
+        vertical_center: {
+            width: "100%",
+            margin: "0",
+            position: "absolute",
+            top: "50%",
+            transform: "translateY(-50%)",
+          },
+          paper: {
+            margin: theme.spacing(8, 4),
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          },
+      }));
+
+    const classes = useStyles();
+
+    const [form, setForm] = useState({
+        name: "",
+        info: "",
+        desc: "",
+      });
+
+    const handleChange = (e) => {
+        const { value, id } = e.target;
+        setForm({ ...form, [id]: value });
+        e.preventDefault();
+      };
+
+    const submitNewList = async event => {
       event.preventDefault()
+      console.log("TestFront");
   
       const res = await fetch('/api/registerTopList', {
         body: JSON.stringify({
-          name: event.target.name.value
+          name: form.name,
+          info: form.info,
+          desc: form.desc
         }),
         headers: {
           'Content-Type': 'application/json'
@@ -17,11 +66,53 @@ const NewTopList = () => {
     }
   
     return (
-      <form onSubmit={testFunction}>
-        <label htmlFor="name">Name</label>
-        <input id="name" name="name" type="text" autoComplete="name" required />
-        <button type="submit">Register</button>
-      </form>
+    <Grid container rowSpacing={2} columnSpacing={1} justifyContent = "center" className={classes.vertical_center} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+        <FormControl>
+            <Grid item xs={12}>
+            <Typography variant="h4" component="div" gutterBottom>
+                Nueva TopList
+            </Typography>
+            </Grid>
+            <Grid item xs={12}>
+            <TextField 
+              id="name" 
+              label="name" 
+              variant="filled" 
+              fullWidth="true"
+              value={form.name}
+              onChange={handleChange}
+            />
+            </Grid>
+            <Grid item xs={12}>
+            <TextField 
+              id="info" 
+              label="info" 
+              variant="filled" 
+              fullWidth="true"
+              value={form.info}
+              onChange={handleChange}
+            />
+            </Grid>
+            <Grid item xs={12}>
+            <TextField 
+              id="desc" 
+              label="desc" 
+              variant="filled" 
+              fullWidth="true"
+              value={form.desc}
+              onChange={handleChange}
+            />
+            </Grid>
+            <Button 
+            onClick={submitNewList} 
+            variant="contained" 
+            color="primary">
+                Submit
+            </Button>
+        </FormControl>
+        </div>
+    </Grid>
     )
   };
   
