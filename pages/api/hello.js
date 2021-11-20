1fab0543401b2689879f2cc9294239cbb2c3e036
Yugnar <yugnarcorp@hotmail.com>
2021-11-19 19:06:26 -0600

    Api connection to MoviesAPI and SteamAPI

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ url: "https://cdn.cloudflare.steamstatic.com/steam/apps/1276790/header.jpg?t=1637186196" });
 }
